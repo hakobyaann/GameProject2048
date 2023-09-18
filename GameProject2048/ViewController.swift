87a1010bfe00b6202af5917901a059b9748ab882
hakobyaann <hakobyan.anna@student.rau.am>
2023-09-18 18:20:47 +0400

    Initial Commit

---
+++ GameProject2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameProject2048
+//
+//  Created by Anna Hakobyan on 18.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
